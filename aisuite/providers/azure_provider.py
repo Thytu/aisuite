59ec7f8b0a72348ee22aed770229b286cbb8c563
Valentin De Matos <vltn.dematos@gmail.com>
2024-12-23 14:40:30 +0100

    fix azure provider
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- aisuite/providers/azure_provider.py
+++ aisuite/providers/azure_provider.py
@@ -10,6 +10,7 @@ class AzureProvider(Provider):
     def __init__(self, **config):
         self.base_url = config.get("base_url") or os.getenv("AZURE_BASE_URL")
         self.api_key = config.get("api_key") or os.getenv("AZURE_API_KEY")
+        self.api_version = config.get("api_version") or os.getenv("AZURE_API_VERSION") or "2024-02-01"
         if not self.api_key:
             raise ValueError("For Azure, api_key is required.")
         if not self.base_url:
@@ -18,17 +19,14 @@ class AzureProvider(Provider):
             )
 
     def chat_completions_create(self, model, messages, **kwargs):
-        url = f"https://{model}.westus3.models.ai.azure.com/v1/chat/completions"
-        url = f"https://{self.base_url}/chat/completions"
-        if self.base_url:
-            url = f"{self.base_url}/chat/completions"
+        url = f"{self.base_url}/openai/deployments/{model}/chat/completions?api-version={self.api_version}"
 
         # Remove 'stream' from kwargs if present
         kwargs.pop("stream", None)
         data = {"messages": messages, **kwargs}
 
         body = json.dumps(data).encode("utf-8")
-        headers = {"Content-Type": "application/json", "Authorization": self.api_key}
+        headers = {"Content-Type": "application/json", "api-key": self.api_key}
 
         try:
             req = urllib.request.Request(url, body, headers)
